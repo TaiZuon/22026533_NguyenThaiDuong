34cd06bbd9c44f772bd0e846a275e71f24a943ee
Thai Duong <thaiduongabcdef@gmail.com>
2023-05-11 20:03:20 +0700

    L6

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,6 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    (this->color) = color;
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
 
@@ -23,8 +24,8 @@ void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
     double rad = (angle / 180) * M_PI;
-    x += cos(rad) * numPixel;
-    y -= sin(rad) * numPixel;
+    x += (int) (cos(rad) * (double) numPixel);
+    y -= (int) (sin(rad) * (double) numPixel);
 }
 
 
@@ -37,7 +38,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
-    jumpForward(-numPixel);
+    this->jumpForward(-numPixel);
 }
 
 
@@ -46,12 +47,18 @@ void Painter::jumpBackward(int numPixel)
         
     Returns:
         None
-***/       
+***/
+
+void compress(double& angle) {
+    while (angle >= 360) angle -= 360;
+    while (angle < 0) angle += 360;
+}
+
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
-    this->angle += degree;
-    setAngle(fmod(angle, 360));
+    angle += degree;
+    compress(angle);
 }
 
 
@@ -63,8 +70,8 @@ void Painter::turnLeft(double degree)
 ***/     
 void Painter::turnRight(double degree)
 {
-    // TODO: rotate right the painter  
-    turnLeft(-degree); 
+    // TODO: rotate right the painter   
+    this->turnLeft(-degree);
 }
 
 /***  
@@ -75,12 +82,12 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
-    Uint8 r = rand() % 256;
-    Uint8 g = rand() % 256;
-    Uint8 b = rand() % 256;
-    SDL_Color color= {r, g, b};
-    setColor(color);
+    // TODO: set random color
+    int r = rand() % 256;
+    int g = rand() % 256;
+    int b = rand() % 256;
+    SDL_Color color = {r, g, b};
+    setColor(color);    
 }
 
 
